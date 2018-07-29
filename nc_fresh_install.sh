e40943750de577fe542ac2f97708160a0449cc98
T.M. Wrath <meltontyler14@gmail.com>
2018-07-29 06:20:52 -0400

    Update nc_fresh_install.sh

--- nc_fresh_install.sh
+++ nc_fresh_install.sh
@@ -28,7 +28,7 @@ sudo mv nginx.conf nginx.conf.backup
 wget -O nginx.conf https://goo.gl/n8crcR
 sudo mkdir /var/www/"$DOMAIN"
 cd ~
-wget -O nextcloud.zip https://download.nextcloud.com/server/releases/nextcloud-13.0.1.zip
+wget -O nextcloud.zip https://download.nextcloud.com/server/releases/nextcloud-13.0.5.zip
 unzip nextcloud.zip
 mv /root/nextcloud/* /var/www/"$DOMAIN"/
 rm -rf nextcloud nextcloud.zip
